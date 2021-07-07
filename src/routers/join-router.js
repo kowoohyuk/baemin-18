375dc319d08bc89e5b540a9b688c1e6b551e7d6a
kowoohyuk <kowoohyuk91@gmail.com>
2021-07-08 01:19:30 +0900

    [#12] wip: :gear: join-2 UI 구현 완료
    - 이벤트 기능 미작성
    - css 파일 분리 필요 (리팩토링)

--- src/routers/join-router.js
+++ src/routers/join-router.js
@@ -2,7 +2,9 @@ import express from 'express';
 
 const joinRouter = express.Router();
 
-joinRouter.get('/', (req, res) => res.render('join', { title: '회원가입' }));
+joinRouter.get('/2', (req, res) => res.render('join-2', { title: '회원가입' }));
+
+joinRouter.get('/3', (req, res) => res.render('join-3', { title: '회원가입' }));
 
 joinRouter.post('/', (req, res) => {
   // 회원가입 처리
