e7b47fa134d9fae8461bc88840e16925923b201f
jihoMoon <sun_days@naver.com>
2021-07-08 21:06:46 +0900

    [#29] 권한 인증
    1. jwt를 decode하여 유저와 일치하는지 확인
    2. user가 로그인해있지않으면 닉네임 표시 X

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,37 +1,20 @@
-import Nedb from 'nedb';
-import jwt from 'jsonwebtoken';
-
-const db = new Nedb({
-    filename : 'src/models/User.db', // DB 파일의 위치 및 이름
-    autoload : true // DB 생성시 파일 로드
-});
-
-const findByToken = (token, cb) => {
-
-    jwt.verify(token,'secret', (err, decode) => {
-        db.find({"_id":decode, "token":token}, (err, users) => {
-            if(err) return cb(err);
-            cb(null, users);
-        })
-    })
-}
+import { findByToken } from '../apis/token.js'
 
 let auth = (req, res, next) => {
   let token = req.cookies.w_auth;
 
   findByToken(token, (err, users) => {
-    if (err) return console.log(err);
-    if (users.length === 0) res.redirect('/login');
+    if (err) return res.redirect('/error');
+    if (users.length === 0) return res.render('main', { title: 'My배민' });
 
     const user = users[0];
-    
+
     req.token = token;
     req.user = user;
-    
+  
     next();
   });
 };
 
-module.exports = { auth };
-
+export { auth }
 
