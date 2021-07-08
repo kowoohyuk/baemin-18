f9c161f04dde532481589494af59385c16aa4a25
kowoohyuk <kowoohyuk91@gmail.com>
2021-07-08 11:06:10 +0900

    [#24] refactor: :hammer: 파일명 및 router path 수정
    - index.js
       - join path 수정
    - join... .pug
       - 명확한 파일명으로 수정

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express from 'express';
 import loginRouter from './routers/login-router.js';
 import joinRouter from './routers/join-router.js';
 import path from 'path';
@@ -7,16 +7,15 @@ const app = express();
 const __dirname = path.resolve();
 
 app.use('/js', express.static(path.join(__dirname, 'src', 'js')));
-app.set('views', 'src/views')
+app.use('/public', express.static(path.join(__dirname, 'src', 'public')));
+app.set('views', 'src/views');
 app.set('view engine', 'pug');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get('/', (req, res) => res.render('main', { title: 'My배민' }));
 app.use('/login', loginRouter);
-app.use('/join-1', joinRouter);/* 수정 */
-
-app.use("/public", express.static(path.join(__dirname, 'src' , 'public')));/* 수정 */
+app.use('/join', joinRouter);
 
 const port = process.env.PORT || 5000;
 
--- src/routers/join-router.js
+++ src/routers/join-router.js
@@ -2,9 +2,15 @@ import express from 'express';
 
 const joinRouter = express.Router();
 
-joinRouter.get('/2', (req, res) => res.render('join-2', { title: '회원가입' }));
-joinRouter.get('/3', (req, res) => res.render('join-3', { title: '회원가입' }));
-joinRouter.get('/', (req, res) => res.render('join-1', { title: '회원가입' , leftbtn: 'X'}));
+joinRouter.get('/', (req, res) =>
+  res.render('join-agree', { title: '회원가입' })
+);
+joinRouter.get('/2', (req, res) =>
+  res.render('join-phone', { title: '회원가입' })
+);
+joinRouter.get('/3', (req, res) =>
+  res.render('join-info', { title: '회원가입' })
+);
 
 joinRouter.post('/', (req, res) => {
   // 회원가입 처리
