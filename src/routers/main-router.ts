9b65c432db9da0a2cda1b5e2d04407b2213938cc
kowoohyuk <kowoohyuk91@gmail.com>
2021-08-28 17:28:21 +0900

    [#54] wip: :gear: 메인 라우터 마이그레이션
    - 익스프레스 리퀘스트 타입에 토큰 추가 (optional)

--- src/routers/main-router.ts
+++ src/routers/main-router.ts
@@ -1,17 +1,16 @@
-import express, { Request } from "express";
-import { auth } from "../middleware/auth.js";
-import db from "../database/database.js";
-import { User } from "../types/User.js";
+import express from 'express';
+import auth from '../middleware/auth';
+import db from '../database/database';
 
 const mainRouter = express.Router();
 
-mainRouter.get("/", auth, (req, res) => {
-  res.render("main", { title: "My배민", nickname: req.user.nickname });
+mainRouter.get('/', auth, (req, res) => {
+  res.render('main', { title: 'My배민', nickname: req.user.nickname });
 });
 
-mainRouter.put("/logout", auth, async (req, res) => {
+mainRouter.put('/logout', auth, async (req, res) => {
   try {
-    await db.update({ _id: req.user._id }, { $set: { token: "" } });
+    await db.update({ _id: req.user._id }, { $set: { token: '' } });
     return res.json({
       result: 0,
     });
