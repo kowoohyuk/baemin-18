9166a7e1487f156d77953b6351bafc13568b9a7d
kowoohyuk <kowoohyuk91@gmail.com>
2021-08-28 17:35:55 +0900

    [#54] feat: :sparkles: 백엔드 마이그레이션 작업 완료

--- src/scripts/join.ts
+++ src/scripts/join.ts
@@ -2,15 +2,14 @@ const getVerifyCode = () => {
   const number = Array(4)
     .fill(0)
     .map((_) => Math.floor(Math.random() * 10))
-    .join("");
-  console.log(number);
+    .join('');
 };
 
 const verifyButtonHandler = () => {
   setTimeout(getVerifyCode, 2000);
 };
 
-const verifyButtons = document.querySelectorAll(".btn-verify");
+const verifyButtons = document.querySelectorAll('.btn-verify');
 verifyButtons.forEach((btn) =>
-  btn.addEventListener("click", verifyButtonHandler)
+  btn.addEventListener('click', verifyButtonHandler)
 );
